0b93f0c89275ba769504eaed5265a3f51c9f0d43
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2022-10-14 19:08:11 -0400

    Create skeletons of planned classes

---
+++ src/main/java/cellsociety/State.java
@@ -0,0 +1,5 @@
+package cellsociety;
+
+public interface State {
+  <T extends Enum<T>> T getState();
+}
---
+++ src/main/java/cellsociety/view/CellView.java
@@ -0,0 +1,11 @@
+package cellsociety.view;
+
+import cellsociety.Observer;
+
+public class CellView implements Observer {
+
+  @Override
+  public void update() {
+
+  }
+}
