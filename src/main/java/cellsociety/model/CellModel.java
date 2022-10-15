531706e86e99733f5a8ec08f8832cc97f9470fea
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-10-14 23:55:41 -0400

    added code in Model package (GridModel, CellModel, boilerplate FigureOuterCode)

--- src/main/java/cellsociety/model/CellModel.java
+++ src/main/java/cellsociety/model/CellModel.java
@@ -2,16 +2,95 @@ package cellsociety.model;
 
 import cellsociety.Observable;
 import cellsociety.Observer;
+import cellsociety.State;
 
+import java.util.List;
+
+/**
+ * Contains the current state and next state of cells. Alerts list of Observers
+ * when the current state of this cell changes
+ */
 public class CellModel implements Observable {
+  private State myCurrentState;
+  private State myNextState;
+  private Enum myCellType;
+  private List<Observer> myObservers;
 
-  @Override
-  public void addObserver(Observer observer) {
-
+  /**
+   * Get a new CellModel
+   * @param startingState the starting state of this CellModel.
+   */
+  public CellModel(State startingState) {
+    myCurrentState = startingState;
+    myNextState = startingState;
+    myCellType = startingState.getSimulationType();
   }
 
+  /**
+   * Add an Observer to this CellModel. Whenever the CellModel's state changes,
+   * the Observer will be notified.
+   * @param observer the Observer to be notified
+   */
+  @Override
+  public void addObserver(Observer observer) {
+    myObservers.add(observer);
+  }
+
+  /**
+   * Remove an Observer from this CellModel's list of Observers
+   * @param observer the Observer to be removed
+   */
   @Override
   public void removeObserver(Observer observer) {
+    myObservers.remove(observer);
+  }
 
+  /**
+   * Returns an enum representing the game that this Cell corresponds with
+   * TODO: GIVE THIS METHOD A BETTER NAME?
+   * @return Enum
+   */
+  public Enum getName() {
+    return myCellType;
+  }
+
+  /**
+   * Set current state of this cell. Alerts observers that this cell's state has changed
+   * @param state this cell's new current state
+   */
+  public void setMyCurrentState(State state) {
+    myCurrentState = state;
+    alertMyObservers();
+  }
+
+  /**
+   * Get current state of this cell
+   * @return myCurrentState
+   */
+  public State getMyCurrentState() {
+    return myCurrentState;
+  }
+
+  /**
+   * Get the next state of this cell
+   */
+  public State getMyNextState() {
+    return myNextState;
+  }
+
+  /**
+   * Set next state of this cell
+   * @param state this cell's next state
+   */
+  public void setMyNextState(State state) {
+    myNextState = state;
+  }
+
+  // TODO: Should we put this method in the Observable interface?
+  // TODO: Finish writing this method
+  private void alertMyObservers() {
+    for (Observer observer : myObservers) {
+      // do some action
+    }
   }
 }
--- src/main/java/cellsociety/model/GridModel.java
+++ src/main/java/cellsociety/model/GridModel.java
@@ -1,5 +1,67 @@
 package cellsociety.model;
 
+import cellsociety.State;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Maintains a 2D List of CellModels
+ */
 public class GridModel {
+    List<List<CellModel>> myCells;
+    StateFigureOuter myStateFigureOuter;
+
+    /**
+     * Creates the GridModel
+     * @param cellStateGrid 2D List of initial states the cells will contain
+     * @param stateFigureOuter the StateFigureOuter that determines the states of cells each time
+     *                         GridModel determines the next state of the cells in the grid
+     */
+    public GridModel(List<List<State>> cellStateGrid, StateFigureOuter stateFigureOuter) {
+        loadMyCells(cellStateGrid);
+        myStateFigureOuter = stateFigureOuter;
+    }
+
+    /**
+     * Determine the next state of the grid.
+     */
+    public void iterateCellStates() {
+        determineNextStates();
+        setCurrentStatesToNextStates();
+    }
+
+    private void determineNextStates() {
+        for (int i = 0; i < myCells.size(); i++) {
+            for (int k = 0; k < myCells.get(i).size(); k++) {
+                CellModel currentCell = myCells.get(i).get(k);
+                currentCell.setMyNextState(myStateFigureOuter.figureOutNextState(i, k, myCells));
+            }
+        }
+    }
+
+    private void setCurrentStatesToNextStates() {
+        for (List<CellModel> myCell : myCells) {
+            for (CellModel currentCell : myCell) {
+                currentCell.setMyCurrentState(currentCell.getMyNextState());
+            }
+        }
+    }
+
+    private void loadMyCells(List<List<State>> cellGrid) {
+        myCells = new ArrayList<>();
+        for (List<State> states : cellGrid) {
+            List<CellModel> newRow = new ArrayList<>();
+            loadRow(newRow, states);
+            myCells.add(newRow);
+        }
+    }
+
+    // helper for loadMyCells
+    private void loadRow(List<CellModel> newRow, List<State> states) {
+        for (State state : states) {
+            newRow.add(new CellModel(state));
+        }
+    }
 
 }
