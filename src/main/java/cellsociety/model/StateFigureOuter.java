dde35a4160b0e5cba77cf5697f6b7950aceea273
Mazen Selim <ms949@duke.edu>
2022-10-16 09:52:40 -0400

    replaced figureoutnextstate method to take minimize argument intake, deprecated old version

--- src/main/java/cellsociety/model/StateFigureOuter.java
+++ src/main/java/cellsociety/model/StateFigureOuter.java
@@ -5,5 +5,8 @@ import cellsociety.State;
 import java.util.List;
 
 public interface StateFigureOuter {
+    @Deprecated
     State figureOutNextState(int cellRowIndex, int cellColIndex, List<List<CellModel>> cells);
+
+    State figureOutNextState(State currentState, State[] neighboringStates);
 }
