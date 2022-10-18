883653e3fd3372c61a27d01a5eb49f80be04aa0a
Mazen Selim <ms949@duke.edu>
2022-10-18 17:38:31 -0400

    added commenting to methods in CellSpawner

--- src/main/java/cellsociety/controller/CellSpawner.java
+++ src/main/java/cellsociety/controller/CellSpawner.java
@@ -5,6 +5,7 @@ import cellsociety.model.CellModel;
 import cellsociety.view.CellView;
 import cellsociety.view.GridView;
 
+
 public class CellSpawner {
 
   private final CellModel[][] myCellModels;
@@ -16,6 +17,9 @@ public class CellSpawner {
 
   private final InitialStateReader myInitialStateReader;
 
+  /**
+   * Initializes parallel cell model/view data structures for a given simulation
+   */
   public CellSpawner(GridView gridView, InitialStateReader initialStateReader) {
     myInitialStateReader = initialStateReader;
     myGridView = gridView;
@@ -30,6 +34,9 @@ public class CellSpawner {
     initializeGrid();
   }
 
+  /**
+   * Method to iterate over each cell
+   */
   public void initializeGrid() {
 
     for (int row = 0; row < myNumRows; row++) {
@@ -41,27 +48,50 @@ public class CellSpawner {
     }
   }
 
+  /**
+   * Method to set the initial states of each cell. Links individual cell models to their cell views.
+   * @param row the x value in the [x][y] coordinate of the data structure
+   * @param col the y value in the [x][y] coordinate of the data structure
+   */
   private void initializeCell(int row, int col) {
     State cellState = getState(row, col);
     myCellModels[row][col] = new CellModel(cellState);
-    myCellViews[row][col] = new CellView(myCellModels[row][col]); // will resolve when merged
+    myCellViews[row][col] = new CellView(myCellModels[row][col]);
     myGridView.addCell(myCellViews[row][col], row, col);
     myCellModels[row][col].addObserver(myCellViews[row][col]);
     myCellModels[row][col].setCurrentState(cellState);
   }
 
+  /**
+   * Method to allow outside classes to access a particular cell model
+   * @param row the x value in the [x][y] coordinate of the data structure
+   * @param col the y value in the [x][y] coordinate of the data structure
+   */
   public CellModel getCell(int row, int col) {
     return myCellModels[row][col];
   }
 
+  /**
+   * Method to obtain the length of the cell model/view grid
+   */
   public int getNumRows() {
     return myNumRows;
   }
 
+  /**
+   * Method to obtain the width of the cell model/view grid
+   */
   public int getNumCols() {
     return myNumCols;
   }
 
+  /**
+   * Method to obtain the state of the cell model/view grid. Uses abstraction through
+   * InitialStateReader to translate the state value, e.g. 0 or 1, to the state's enum, e.g.
+   * DEAD or ALIVE.
+   * @param row the x value in the [x][y] coordinate of the data structure
+   * @param col the y value in the [x][y] coordinate of the data structure
+   */
   private State getState(int row, int col) {
     return myInitialStateReader.createStateInstance(row, col);
   }
