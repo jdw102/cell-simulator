200f92410b5d7a65b751d6ad9a90cc1cabce9d4b
Mazen Selim <ms949@duke.edu>
2022-10-17 22:18:35 -0400

    removed simulation type method from gameoflifecellstate

--- src/main/java/cellsociety/cellStates/GameOfLifeCellState.java
+++ src/main/java/cellsociety/cellStates/GameOfLifeCellState.java
@@ -10,18 +10,6 @@ public enum GameOfLifeCellState implements State {
   ALIVE;
 
   /**
-   * TODO: Change this implementation. Should we return another enum? Should this enum be located in a different file,
-   * with other enum options too?
-   *
-   * @return type of game
-   */
-  @Override
-  public Enum getSimulationType() {
-    enum temp {GAME_OF_LIFE}
-    return temp.GAME_OF_LIFE;
-  }
-
-  /**
    * Not currently sure how to implement this. Isn't the value of the enum itself equivalent to "the
    * state" of this enum? If that's the case, I don't see why we need this method. I only know the
    * very basics of enums
