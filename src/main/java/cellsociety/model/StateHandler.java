46342195b82582ffad855ee391582f57d81d1b78
Mazen Selim <ms949@duke.edu>
2022-10-17 20:02:58 -0400

    figureOutNextState method signature to better fit the neighborhood model

--- src/main/java/cellsociety/model/StateHandler.java
+++ src/main/java/cellsociety/model/StateHandler.java
@@ -8,7 +8,10 @@ public interface StateHandler {
     @Deprecated
     State figureOutNextState(int cellRowIndex, int cellColIndex, List<List<CellModel>> cells);
 
+    @Deprecated
     State figureOutNextState(State currentState, State[] neighboringStates);
+
+    State figureOutNextState(Neighborhood currNeighborhood);
 
     State getMapping(int stateValue);
 }
