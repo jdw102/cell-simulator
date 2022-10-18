551ad30ae4fe4f07d014aea4ab20d30f05dafa45
Mazen Selim <ms949@duke.edu>
2022-10-17 20:46:40 -0400

    removed deprecated methods

--- src/main/java/cellsociety/model/StateHandler.java
+++ src/main/java/cellsociety/model/StateHandler.java
@@ -6,12 +6,6 @@ import java.util.List;
 
 public interface StateHandler {
 
-  @Deprecated
-  State figureOutNextState(int cellRowIndex, int cellColIndex, List<List<CellModel>> cells);
-
-  @Deprecated
-  State figureOutNextState(State currentState, State[] neighboringStates);
-
   State figureOutNextState(Neighborhood currNeighborhood);
 
   State getMapping(int stateValue);
