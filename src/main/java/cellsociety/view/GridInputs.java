f182e12b051d51f746f346a0c16e6f37671180ff
Jerry Worthy <jdw102@duke.edu>
2022-10-20 13:45:01 -0400

    removed backward button and slightly changed format of grid inputs

--- src/main/java/cellsociety/view/GridInputs.java
+++ src/main/java/cellsociety/view/GridInputs.java
@@ -25,7 +25,6 @@ public class GridInputs {
   private final HBox container;
   private final PlayButton playButton;
   private final Button forwardButton;
-  private final Button backwardButton;
   private final InputFactory inputFactory;
   private final Timeline animation;
   private final Controller controller;
@@ -45,18 +44,14 @@ public class GridInputs {
         .add(new KeyFrame(Duration.seconds(SECOND_DELAY), e -> stepForward()));
     animation.setRate(DEFAULT_SPEED);
     inputFactory = utils;
-    backwardButton = inputFactory.makeButton("BackwardButton",
-        event -> stepBackward());
     forwardButton = inputFactory.makeButton("ForwardButton",
         event -> stepForward());
     playButton = new PlayButton(utils, animation, this);
-    inputFactory.attachTooltip("BackwardButtonTooltip", backwardButton);
     inputFactory.attachTooltip("ForwardButtonTooltip", forwardButton);
     VBox sliderBox = makeSliderBox();
     Region padderRegion = new Region();
     padderRegion.prefWidthProperty().bind(sliderBox.widthProperty());
-    container = new HBox(padderRegion, backwardButton, playButton.getButton(), forwardButton,
-        sliderBox);
+    container = new HBox(sliderBox, playButton.getButton(), forwardButton);
     container.getStyleClass().add("grid-inputs-container");
   }
 
@@ -105,15 +100,6 @@ public class GridInputs {
     controller.updateState();
   }
 
-  /**
-   * Jumps the animation back by one frame.
-   */
-  private void stepBackward() {
-    Duration time = animation.getCurrentTime().add(Duration.seconds(SECOND_DELAY).negate());
-    if (time.toMillis() > 0) {
-      animation.jumpTo(time);
-    }
-  }
 
   /**
    * Disables or enables the backward and forward buttons.
@@ -121,7 +107,6 @@ public class GridInputs {
    * @param disable if true disables, if false enables
    */
   public void disableStepButtons(Boolean disable) {
-    backwardButton.setDisable(disable);
     forwardButton.setDisable(disable);
   }
 }
