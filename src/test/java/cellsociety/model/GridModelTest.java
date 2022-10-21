beba3b0461e6cc2eeb126805ff65d0f451a83c27
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-10-21 17:04:48 -0400

    merging master branch with ryan

---
+++ src/test/java/cellsociety/model/GridModelTest.java
@@ -0,0 +1,19 @@
+package cellsociety.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GridModelTest {
+
+  @BeforeEach
+  void setup() {
+
+  }
+
+  @Test
+  void updateState() {
+
+  }
+}
