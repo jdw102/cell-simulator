f9d6f8d6b7d55c134729eb6cd001c80cd8908f80
Mazen Selim <ms949@duke.edu>
2022-10-23 11:14:25 -0400

    automated the initial state reader testing process

--- src/test/java/cellsociety/controller/InitialStateReaderTest.java
+++ src/test/java/cellsociety/controller/InitialStateReaderTest.java
@@ -6,24 +6,83 @@ import cellsociety.model.statehandlers.StateHandler;
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class InitialStateReaderTest {
 
+  static String[] validSimTypes = {"GameOfLife"};
 
-  StateHandlerLoader stateHandlerLoaderTester = new StateHandlerLoader();
-  StateHandler stateHandlerTester = stateHandlerLoaderTester.getStateHandler("GameOfLife");
-  File initTestFile = new File("data/game_of_life/blinkers.csv");
+  static Map<String, int[][][]> expectedGrids;
 
-  InitialStateReader initialStateReaderTester = new InitialStateReader(stateHandlerTester, initTestFile);
-
-  InitialStateReaderTest()
-      throws CsvValidationException, IOException, WrongFileTypeException, ClassNotFoundException {
+  static String[] getSimTypes() {
+    return validSimTypes;
+  }
+  @BeforeAll
+  static void setUp() {
+    loadExpectedGrids();
   }
 
-  @Test
-  void getState() {
-    int[][] expected = {
+  @ParameterizedTest
+  @MethodSource("getSimTypes")
+  void getsInitialStates(String mySimType){
+    loadExpectedGrids();
+    StateHandlerLoader stateHandlerLoaderTester = new StateHandlerLoader();
+    StateHandler stateHandlerTester = null;
+    try {
+      stateHandlerTester = stateHandlerLoaderTester.getStateHandler(mySimType);
+      System.out.println("got state handler " + stateHandlerTester != null);
+    } catch (Exception e) {
+      // do nothing
+    }
+
+    for(int i = 0; i < 3; i ++) {
+      String filename = mySimType.toLowerCase() + "cellstatestest";
+
+
+      File myFile = new File("src/test/resources/" + filename + i + ".csv");
+
+      InitialStateReader stateReaderTester = null;
+
+      try {
+        stateReaderTester = new InitialStateReader(stateHandlerTester,
+            myFile);
+      } catch (Exception e) {
+        // do nothing
+      }
+      int[][] expected = expectedGrids.get(mySimType)[i];
+
+      for(int row = 0; row < expected.length; row++) {
+        for(int col = 0; col < expected[row].length; col++) {
+          String expectedString = mySimType + ", expectedGrids[" + i + "], " + "row: " + row + " col: " + col +" = " +
+              expected[row][col];
+          String actualString = mySimType + ", expectedGrids[" + i + "], " + "row: " + row + " col: " + col +" = " +
+              stateReaderTester.getStateValue(row, col);
+          assertEquals(expectedString, actualString);
+        }
+      }
+
+      assertEquals(expected.length, stateReaderTester.getNumRows());
+      assertEquals(expected[0].length, stateReaderTester.getNumCols());
+
+    }
+
+  }
+
+
+  /**
+   * Part of the set up sequence, each simulation should have 3 test csvs, numbered 0-2.
+   */
+
+  static void loadExpectedGrids() {
+    expectedGrids = new HashMap<>();
+
+    int[][] golExpected1 = {
         {0,0,0,0,0,0,0,1,1,1},
         {0,0,0,0,0,0,0,0,0,0},
         {1,0,0,0,0,0,0,0,0,0},
@@ -35,20 +94,63 @@ class InitialStateReaderTest {
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,1,1,1,0,0,0},
     };
-    for(int row = 0; row < expected.length; row++) {
-      for(int col = 0; col < expected[row].length; col++) {
-        assertEquals(expected[row][col], initialStateReaderTester.getStateValue(row, col));
-      }
-    }
+    int[][] golExpected2 = {
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,1,0,1,0,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,0,1,0,1,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0}
+    };
+    int[][] golExpected3 = {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0},
+        {0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    };
+    expectedGrids.put("GameOfLife", new int[][][]{golExpected1, golExpected2, golExpected3});
   }
 
-  @Test
-  void getNumRows() {
-    assertEquals(10, initialStateReaderTester.getNumRows());
-  }
-
-  @Test
-  void getNumCols() {
-    assertEquals(10, initialStateReaderTester.getNumRows());
-  }
 }
