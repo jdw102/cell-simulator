9a36d40831f4177cf0b5db414eb412429cd031a1
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-10-25 00:38:51 -0400

    preparing to merge with master

--- src/test/java/cellsociety/model/GameOfLifeStateHandlerTest.java
+++ src/test/java/cellsociety/model/GameOfLifeStateHandlerTest.java
@@ -9,13 +9,6 @@ import org.junit.jupiter.api.Test;
 
 public class GameOfLifeStateHandlerTest {
 
-  /*
-  options:
-    load file into a neighborhood
-    call figureoutnextstate on that neighborhood
-    load correct output into neighborhood and make sure they match
-   */
-
   @Test
   void testFigureOutNextState() throws IOException, CsvValidationException {
 //    File neighborhoodFile = new File("test.txt");
@@ -35,14 +28,9 @@ public class GameOfLifeStateHandlerTest {
       }
     } catch (NullPointerException e) {
       // do nothing
-      System.out.println(i);
+//      System.out.println(i);
     }
 
-    //    File testFile = new File(DEFAULT_RESOURCE_FOLDER + "game_of_life/blinkers.csv");
-
-//    System.out.println("yeeeee");
-//    reader.readNext();
-//    System.out.println(reader.readNext()[0]);
   }
 
 
