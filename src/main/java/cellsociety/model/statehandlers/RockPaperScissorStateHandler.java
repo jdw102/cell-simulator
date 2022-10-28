ba979496abf3bc5aee307b281eb53947855ea436
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2022-10-28 17:11:40 -0400

    Update map to be static final

--- src/main/java/cellsociety/model/statehandlers/RockPaperScissorStateHandler.java
+++ src/main/java/cellsociety/model/statehandlers/RockPaperScissorStateHandler.java
@@ -11,15 +11,14 @@ public class RockPaperScissorStateHandler extends StateHandler {
   private static final String STATES_PACKAGE = "cellsociety.cellstates.rockpaperscissorcellstates.";
   private static final String HANDLER_NAME = "RockPaperScissorStateHandler";
 
-  private Map<RockPaperScissorCellState, RockPaperScissorCellState> enemiesMap;
+  private static final Map<RockPaperScissorCellState, RockPaperScissorCellState> enemiesMap = Map.of(
+      RockPaperScissorCellState.SCISSOR, RockPaperScissorCellState.ROCK,
+      RockPaperScissorCellState.ROCK, RockPaperScissorCellState.PAPER,
+      RockPaperScissorCellState.PAPER, RockPaperScissorCellState.SCISSOR
+  );
 
   public RockPaperScissorStateHandler() throws RuntimeException {
     super(RockPaperScissorCellState.class, HANDLER_NAME, STATES_PACKAGE);
-    enemiesMap = Map.of(
-        RockPaperScissorCellState.SCISSOR, RockPaperScissorCellState.ROCK,
-        RockPaperScissorCellState.ROCK, RockPaperScissorCellState.PAPER,
-        RockPaperScissorCellState.PAPER, RockPaperScissorCellState.SCISSOR
-    );
   }
 
   private State play(Enum currState, int numEnemiesSurrounding) {
