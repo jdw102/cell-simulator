f21722e94b865b149560b0958f726028412b2da2
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-10-30 15:54:00 -0400

    added javadoc

--- src/main/java/cellsociety/controller/GridModelLoader.java
+++ src/main/java/cellsociety/controller/GridModelLoader.java
@@ -13,11 +13,27 @@ import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
+/**
+ * Loads the correct GridModel for this simulation type. The pairings between simulation
+ * types and GridModels are located in gridmodel_chooser.properties in the resources folder
+ *
+ * @author Ryan Wolfram
+ */
 public class GridModelLoader {
 
   public static final String RESOURCE_FILE_NAME = "gridmodel_chooser";
   public static final String GRID_MODEL_PACKAGE_PATH = DEFAULT_RESOURCE_PACKAGE + "model.";
 
+  /**
+   * Returns the correct GridModel for this simulation type. The pairings between simulation
+   * types and GridModels are located in a properties file in the resources folder. If no
+   * pairing for this simulation type exists in the properties file, return a DefaultGridModel.
+   *
+   * @param simType the type of simulation
+   * @param neighborhoodsLoader the NeighborhoodsLoader passed to new GridModel
+   * @param stateHandler the StateHandler passed to the new GridModel
+   * @return GridModel
+   */
   public GridModel getGridModel(String simType, NeighborhoodsLoader neighborhoodsLoader,
       StateHandler stateHandler) {
 
@@ -33,6 +49,8 @@ public class GridModelLoader {
 
   }
 
+
+  // use reflection to get the correct GridModel instance
   private GridModel getGridModelInstance(String modelType,
       NeighborhoodsLoader neighborhoodsLoader, StateHandler stateHandler) {
     try {
@@ -47,6 +65,7 @@ public class GridModelLoader {
     }
   }
 
+  // load a Collection of all simTypes in the properties file
   private Collection<String> getSimTypes(ResourceBundle mapping) {
     Enumeration<String> en = mapping.getKeys();
     Collection<String> simTypes = new HashSet<>();
--- src/main/java/cellsociety/model/statehandlers/SegregationStateHandler.java
+++ src/main/java/cellsociety/model/statehandlers/SegregationStateHandler.java
@@ -4,15 +4,27 @@ import cellsociety.State;
 import cellsociety.cellstates.segregationcellstates.SegregationCellState;
 import cellsociety.model.Neighborhood;
 
+/**
+ * StateHandler that implements the transition function rules for the Segregation simulation
+ */
 public class SegregationStateHandler extends StateHandler {
   private static final String STATES_PACKAGE = "cellsociety.cellstates.segregationcellstates.";
   private static final String HANDLER_NAME = "SegregationStateHandler";
   public static final double THRESHOLD = 0.3;   // TODO: Read this in from SIM params!
 
+  /**
+   * Get a new SegregationStateHandler
+   * @throws RuntimeException
+   */
   public SegregationStateHandler() throws RuntimeException {
     super(SegregationCellState.class, HANDLER_NAME, STATES_PACKAGE);
   }
 
+  /**
+   * Return the next state of this neighborhood
+   * @param neighborhood The current neighborhood being examined to determine next state of
+   * @return
+   */
   @Override
   public State figureOutNextState(Neighborhood neighborhood) {
     // return EMPTY if already EMPTY
@@ -22,7 +34,7 @@ public class SegregationStateHandler extends StateHandler {
     }
 
     int numCurrStateEnum = neighborhood.count(currStateEnum);
-    int total = countNonEmptyAgents(neighborhood);
+    int total = countNonEmptyNeighbors(neighborhood);
 
     if (total == 0 || (double) numCurrStateEnum / total >= THRESHOLD) {
       return getStateInstance(currStateEnum);
@@ -32,7 +44,8 @@ public class SegregationStateHandler extends StateHandler {
     }
   }
 
-  private int countNonEmptyAgents(Neighborhood neighborhood) {
+  // count the total number of Neighbors that aren't EMPTY
+  private int countNonEmptyNeighbors(Neighborhood neighborhood) {
     int count = 0;
     for (SegregationCellState cellStateEnum : SegregationCellState.values()) {
       if (!cellStateEnum.equals(SegregationCellState.EMPTY)) {
