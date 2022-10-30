e8aeb4ec5a96876bfe6c8e7e24231a27082021b9
Ryan Wolfram <ryan.wolfram@duke.edu>
2022-10-30 17:20:08 -0400

    added instance variables in wator state classes

--- src/main/java/cellsociety/cellstates/wator/FishState.java
+++ src/main/java/cellsociety/cellstates/wator/FishState.java
@@ -2,7 +2,7 @@ package cellsociety.cellstates.wator;
 
 import cellsociety.State;
 
-public class FishState implements State {
+public class FishState extends Animal implements State {
   @Override
   public Enum getStateEnum() {
     return WatorCellState.FISH;
--- src/main/java/cellsociety/cellstates/wator/SharkState.java
+++ src/main/java/cellsociety/cellstates/wator/SharkState.java
@@ -2,9 +2,21 @@ package cellsociety.cellstates.wator;
 
 import cellsociety.State;
 
-public class SharkState implements State {
+public class SharkState extends Animal implements State {
+  public static final int STARTING_ENERGY_LEVEL = 3;
+  private int energyCounter = STARTING_ENERGY_LEVEL;
+
   @Override
   public Enum getStateEnum() {
     return WatorCellState.SHARK;
   }
+
+  public int getEnergyCounter() {
+    return energyCounter;
+  }
+
+  public void setEnergyCounter(int num) {
+    energyCounter = num;
+  }
+
 }
